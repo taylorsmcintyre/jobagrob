ba08d754904ec39702538e04d9134655fc3a896a
taylorsmcintyre <tsm.mac@icloud.com>
2013-10-10 10:34:51 -0700

    new abstract model for accounts

--- api/user-model.js
+++ api/user-model.js
@@ -1,10 +1,11 @@
-/* Creation of Server */
 var mongoose = require('mongoose'),
+    extend = require('mongoose-schema-extend'),
     Schema = mongoose.Schema,
     bcrypt = require('bcrypt'),
-    SALT_WORK_FACTOR = 10;
+    SALT_WORK_FACTOR = 10,
+    accountSchema = require('./account-abstract-model');
 
-var userSchema = new Schema({
+var userSchema = accountSchema.extend({
     firstName: {
         type: String,
         required: true
@@ -12,46 +13,8 @@ var userSchema = new Schema({
     lastName: {
         type: String,
         required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        index: {
-            unique: true
-        }
-    },
-    password: {
-        type: String,
-        required: true
     }
 });
 
-userSchema.pre('save', function (next) {
-    var user = this;
-
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
-
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-        if (err) return next(err);
-
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function (err, hash) {
-            if (err) return next(err);
-
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
-    });
-});
-
-userSchema.methods.comparePassword = function (candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
-};
 
 module.exports = mongoose.model('User', userSchema);
